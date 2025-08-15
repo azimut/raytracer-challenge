6dff0cbeb2661a8a82b27d7728c89801a482b8ee
azimut <azimut.github@protonmail.com>
2025-08-14 21:24:08 -0300

    add chapter 4 transformation.c/.h

---
+++ transformation.h
@@ -0,0 +1,13 @@
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+#include "./matrix.h"
+
+Mat4 translation(float, float, float);
+Mat4 scaling(float, float, float);
+Mat4 rotation_x(float);
+Mat4 rotation_y(float);
+Mat4 rotation_z(float);
+float radians(float);
+
+#endif /* TRANSLATION_H */
