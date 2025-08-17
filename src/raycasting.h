f024c18cd287a0da27efe8c5a217d87bc0e487d4
azimut <azimut.github@protonmail.com>
2025-08-16 22:32:51 -0300

    refactor

--- src/raycasting.h
+++ src/raycasting.h
@@ -1,5 +1,6 @@
 #ifndef RAYCASTING_H
 #define RAYCASTING_H
+
 #include "./tuple.h"
 #include <stdlib.h>
 
@@ -7,28 +8,23 @@ typedef struct Ray {
   Point origin;
   Vector direction;
 } Ray;
-
-Ray ray(Point, Tuple);
-Point position(Ray, float);
-
 typedef struct Sphere {
   int id;
 } Sphere;
-Sphere sphere();
-
 typedef struct Intersection {
   float t;
   Sphere object;
 } Intersection;
-Intersection intersection(float, Sphere);
-
 typedef struct Intersections {
   size_t count;
   Intersection *hits;
 } Intersections;
 
+Ray ray(Point, Tuple);
+Point position(Ray, float);
+Sphere sphere();
+Intersection intersection(float, Sphere);
 Intersections intersect(Sphere, Ray);
-
 void free_intersections(Intersections);
 
 #endif /* RAYCASTING_H */
