5d71f5fa3073887c30d6501b1512378e2c1370f0
azimut <azimut.github@protonmail.com>
2025-08-22 02:52:15 -0300

    util: add clamp()

--- lib/util.c
+++ lib/util.c
@@ -2,4 +2,8 @@
 #include <math.h>
 #include <stdbool.h>
 
+float clamp(float value, float min, float max) {
+  return fmin(fmax(value, min), max);
+}
+
 bool near(float x, float y) { return fabs(x - y) < EPSILON; }
