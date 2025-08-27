b8566fa4ae2be6a6f6a2d97ac85ad48acf1b3a03
azimut <azimut.github@protonmail.com>
2025-08-26 21:11:47 -0300

    shading: commented out attenuation

--- lib/shading.c
+++ lib/shading.c
@@ -30,7 +30,10 @@ Color lighting(MaterialPhong material, Point point, PointLight light,
   Vector dir_light = tuple_normalize(tuple_sub(light.position, point));
   float light_dot_normal = tuple_dot_product(dir_light, normal);
   Color diffuse = {0, 0, 0}, specular = {0, 0, 0};
+  float attenuation = 1;
   if (!in_shadow && light_dot_normal >= 0) {
+    /* double distance = tuple_length(tuple_sub(light.position, point)); */
+    /* attenuation = 1 / (1.0 + 0.14 * distance + 0.07 * distance); */
     diffuse = color_smul(effective_color, material.diffuse * light_dot_normal);
     Vector reflection = reflect(tuple_neg(dir_light), normal);
     float reflect_dot_eye = tuple_dot_product(reflection, eye);
@@ -39,5 +42,6 @@ Color lighting(MaterialPhong material, Point point, PointLight light,
       specular = color_smul(light.intensity, material.specular * factor);
     }
   }
-  return color_add(color_add(ambient, diffuse), specular);
+  return color_add(color_smul(color_add(specular, diffuse), attenuation),
+                   ambient);
 }
