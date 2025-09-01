c67845fec5f2fe50bc310fe0013364c586ac1b68
azimut <azimut.github@protonmail.com>
2025-09-01 15:38:25 -0300

    dev: update dir-locals with gdbpath and 10patterns

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
             (dape-configs . ((gdb-test-unit
                               modes (c-mode)
                               command-cwd dape-command-cwd
-                              command "gdb"
+                              command "/usr/local/bin/gdb"
                               command-args ("--interpreter=dap")
                               compile "make -B build/unit DEBUG=1"
                               :request "launch"
@@ -10,8 +10,16 @@
                              (gdb-5raysphere
                               modes (c-mode)
                               command-cwd dape-command-cwd
-                              command "gdb"
+                              command "/usr/local/bin/gdb"
                               command-args ("--interpreter=dap")
                               compile "make -B build/5raysphere DEBUG=1"
                               :request "launch"
-                              :program "build/5raysphere"))))))
+                              :program "build/5raysphere")
+                             (gdb-10pattern
+                              modes (c-mode)
+                              command-cwd dape-command-cwd
+                              command "/usr/local/bin/gdb"
+                              command-args ("--interpreter=dap")
+                              compile "make -B build/10pattern DEBUG=1 DIMENSION=100"
+                              :request "launch"
+                              :program "build/10pattern"))))))
