0adab63d77c3c69a2a5df3487cc3eb79878115cb
azimut <azimut.github@protonmail.com>
2025-09-03 11:18:06 -0300

    dev: update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,15 +6,8 @@
                               command-args ("--interpreter=dap")
                               compile "make -B build/unit DEBUG=1"
                               :request "launch"
-                              :program "build/unit")
-                             (gdb-5raysphere
-                              modes (c-mode)
-                              command-cwd dape-command-cwd
-                              command "/usr/local/bin/gdb"
-                              command-args ("--interpreter=dap")
-                              compile "make -B build/5raysphere DEBUG=1"
-                              :request "launch"
-                              :program "build/5raysphere")
+                              :program "build/unit"
+                              :stopAtBeginningOfMainSubprogram t)
                              (gdb-10pattern
                               modes (c-mode)
                               command-cwd dape-command-cwd
@@ -22,4 +15,5 @@
                               command-args ("--interpreter=dap")
                               compile "make -B build/10pattern DEBUG=1 DIMENSION=100"
                               :request "launch"
-                              :program "build/10pattern"))))))
+                              :program "build/10pattern"
+                              :stopAtBeginningOfMainSubprogram t))))))
